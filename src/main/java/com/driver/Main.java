dfcce5c980f3d6c40225c982457743392bcc7b1e
Vedant Walavalkar <vedant.walavalkar@gmail.com>
2023-06-09 12:59:16 +0530

    Added Name

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
-  
+  public static void main(String[] args) {
+    RWOnly obj = new RWOnly("Vedant");
+    obj.setName("Vedant Walavalkar");
+    System.out.println(obj.getName());
+  }
 }
